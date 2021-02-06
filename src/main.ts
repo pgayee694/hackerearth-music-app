400628f83c9dbf444d4f1b7dab0a1336377f274c
pgayee694 <36524925+pgayee694@users.noreply.github.com>
2021-02-06 13:28:25 -0800

    more project setup stuff

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config({
+    path: '../../.env',
+  });
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
