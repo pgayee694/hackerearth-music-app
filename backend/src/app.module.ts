f1d6d7da04ed4d9cba0d1406bd07415110e85a38
Josh Grossman <josh.l.grossman@gmail.com>
2021-02-07 16:52:20 -0600

    feat(weather): add weather service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { HttpModule, Module } from '@nestjs/common';
+import { SongController } from './controllers/song.controller';
+import { ConfigProvider } from './providers/config.provider';
+import { EnvironmentProvider } from './providers/environment.provider';
+import { WeatherService } from './services/weather.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [HttpModule],
+  controllers: [SongController],
+  providers: [ConfigProvider, EnvironmentProvider, WeatherService],
 })
 export class AppModule {}
