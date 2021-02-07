aaaf9cfce869d7936d0ac414fbef02d15ca0eb4a
Jason Stratman <jason.stratman@ncr.com>
2021-02-07 10:16:42 -0600

    feat(ui): add some basic UI elements (#2)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { Box, ChakraProvider } from '@chakra-ui/react';
+import React from 'react';
+import { Home } from './home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Box className="App" width="100%" height="100%">
+        <Home />
+      </Box>
+    </ChakraProvider>
   );
 }
 
