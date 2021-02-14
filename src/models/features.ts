2dd17d22e84574c0a7f49337713144dcfe58636f
pgayee694 <36524925+pgayee694@users.noreply.github.com>
2021-02-13 17:28:34 -0800

    added bing api

---
+++ src/models/features.ts
@@ -0,0 +1,15 @@
+export enum Feature {
+  Bay = 'Bay',
+  Beach = 'Beach',
+  Canal = 'Canal',
+  Forest = 'Forest',
+  Island = 'Island',
+  Lake = 'Lake',
+  Mountain = 'Mountain',
+  Plateau = 'Plateau',
+  Reserve = 'Reserve',
+  River = 'River',
+  Sea = 'Sea',
+  Valley = 'Valley',
+  Plain = 'Plain',
+}
