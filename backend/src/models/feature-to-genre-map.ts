a586de689499fe140b74c916ed5094e7b97b9ef7
pgayee694 <36524925+pgayee694@users.noreply.github.com>
2021-02-14 02:53:15 -0800

    added feature to genre map

---
+++ backend/src/models/feature-to-genre-map.ts
@@ -0,0 +1,4 @@
+import { Feature } from './features';
+import { Genres } from './spotify';
+
+export type FeatureToGenreMap = Map<Feature, Genres[]>;
---
+++ backend/src/providers/feature-to-genre-map.provider.ts
@@ -0,0 +1,39 @@
+import { Provider } from '@nestjs/common';
+import { FeatureToGenreMap } from 'src/models/feature-to-genre-map';
+import { Feature } from 'src/models/features';
+import { Genres } from 'src/models/spotify';
+
+export const FeatureToGenreMapToken = Symbol('FeatureToGenreMapToken');
+
+export const FeatureToGenreMapProvider: Provider<FeatureToGenreMap> = {
+  provide: FeatureToGenreMapToken,
+  useValue: new Map([
+    [
+      Feature.Plain,
+      [Genres.Rock, Genres.Pop, Genres.Soundtracks, Genres.RoadTrip],
+    ],
+    [
+      Feature.Bay,
+      [Genres.Rock, Genres.RockNRoll, Genres.Metal, Genres.Industrial],
+    ],
+    [Feature.Beach, [Genres.Summer]],
+    [
+      Feature.Forest,
+      [Genres.Soundtracks, Genres.Classical, Genres.Guitar, Genres.Piano],
+    ],
+    [Feature.Island, [Genres.Summer]],
+    [
+      Feature.Lake,
+      [Genres.Jazz, Genres.Classical, Genres.Disney, Genres.Piano],
+    ],
+    [
+      Feature.Mountain,
+      [Genres.Rock, Genres.Metal, Genres.Opera, Genres.DrumAndBass],
+    ],
+    [Feature.Plateau, [Genres.Rock, Genres.Metal, Genres.Opera]],
+    [Feature.Reserve, []], // idk,
+    [Feature.River, [Genres.Pop, Genres.Piano, Genres.Soul]],
+    [Feature.Sea, [Genres.RockNRoll, Genres.ShowTunes]],
+    [Feature.Valley, [Genres.Ambient]],
+  ]),
+};
